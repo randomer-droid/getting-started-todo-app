a40e84b1f90f52c52160d35d7d78018ebe0e9d9c
randomer-droid <d_bobrovnikov@mail.ru>
2026-07-30 08:43:51 +0100

    change by Claude

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.scss';
 
-// this is a dummy comment to test out git
-// another comment for VS code
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
