1d519430e9e8f9a7688b3d604d4ea2dd856c9ae5
randomer-droid <d_bobrovnikov@mail.ru>
2026-07-30 08:49:24 +0100

    change via GitHub Desktop

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.scss';
 
+// final comment to test out GitHub Desktop
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
